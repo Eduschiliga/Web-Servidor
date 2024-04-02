af027f1f8433f70d1d20f7842ed244c8c4ced104
Eduardo Schiliga <103791066+Eduschiliga@users.noreply.github.com>
2024-04-02 19:00:05 -0300

    commit

---
+++ view/login.view.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Login</title>
+
+    </head>
+    <body>
+        <main>
+            <h1>Login</h1>
+            <form action="" method="post">
+                <label for="email">Usuário</label>
+                <input type="email" name="email" id="email" required placeholder="Informe seu usuário">
+                <label for="password">Senha</label>
+                <input type="password" name="password" id="password" required placeholder="Informe a sua senha">
+                <button type="submit">Entrar</button>
+            </form>
+        </main>
+    </body>
+</html>
