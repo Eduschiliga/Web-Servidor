4138a8e5d835e81d3743af429e748d9de719c885
Eduardo Schiliga <103791066+Eduschiliga@users.noreply.github.com>
2024-04-02 20:05:08 -0300

    commit

---
+++ views/header.view.php
@@ -0,0 +1,16 @@
+<header>
+    <section>
+        <span>Nome de user</span>
+        <span>OAB: 111.111</span>
+    </section>
+
+    <section>
+        <form method="get" action="">
+            <label>
+                Buscar processo
+                <input type="text" name="termo_busca" placeholder="Informe o número do processo...">
+            </label>
+            <button type="submit"><img src="../images/icons/search_icon.png" alt="Ícone de busca"></button>
+        </form>
+    </section>
+</header>
