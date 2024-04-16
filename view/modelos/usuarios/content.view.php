c9918eb196c864dc19b3dd2842d426d3262c2e30
Eduardo Schiliga <103791066+Eduschiliga@users.noreply.github.com>
2024-04-16 18:31:54 -0300

    Alteração tela de Usuários

--- view/modelos/usuarios/content.view.php
+++ view/modelos/usuarios/content.view.php
@@ -1,4 +1,5 @@
 <main>
     <h1>Perfil do Usuário</h1>
-
+    <p>Nome: <?= $usuario->getNome(); ?></p>
+    <p>OAB: <?= $usuario->getOab(); ?></p>
 </main>
--- view/pages/usuarios/usuarios_page.php
+++ view/pages/usuarios/usuarios_page.php
@@ -9,9 +9,7 @@
         <link rel="stylesheet" type="text/css" href="../../styles/global.css"/>
         <link rel="stylesheet" type="text/css" href="../../styles/header.css"/>
         <link rel="stylesheet" type="text/css" href="../../styles/footer.css"/>
-        <link rel="stylesheet" type="text/css" href="../../styles/table.css"/>
         <link rel="stylesheet" type="text/css" href="../../styles/processos.css"/>
-        <link rel="stylesheet" type="text/css" href="../../styles/form.css"/>
         <link rel="icon" type="image/x-icon" href="../../../images/icons/favicon.ico">
     </head>
     <body>
