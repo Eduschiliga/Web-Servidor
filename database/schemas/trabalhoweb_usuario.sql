7d60f1936f96ff924ec7c26e8943770034fb765f
Marcos Gomes <marcosartemio221@gmail.com>
2024-05-14 18:28:46 -0300

    Update table usuario
    Insert data

--- database/schemas/trabalhoweb_usuario.sql
+++ database/schemas/trabalhoweb_usuario.sql
@@ -33,6 +33,16 @@ CREATE TABLE `usuario` (
   UNIQUE KEY `idusuario_UNIQUE` (`idusuario`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuario`
+--
+
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+INSERT INTO `usuario` VALUES (1,'Admin','99999','123456789');
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -43,4 +53,4 @@ CREATE TABLE `usuario` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-05-13 22:36:52
+-- Dump completed on 2024-05-14 18:27:42
